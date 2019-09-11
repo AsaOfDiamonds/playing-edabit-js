03965dd5fb36a093a853d6c89a1712b34f917ce0
AsaOfDiamonds <asaofdiamonds@gmail.com>
2019-09-11 16:57:20 -0600

    started veryEasy.js

---
+++ veryEasy.js
@@ -0,0 +1,16 @@
+//Create a function that finds the index of a given item.
+// If the item is not present, return -1.
+
+function search(arr, item) {
+	return arr.indexOf(item);
+}
+
+//alt answers
+// function search(arr, item) {
+// 	for(let i = 0; i<arr.length; i++){
+// 		if(arr[i] === item) return i;
+// 	}
+// 	return -1;
+// }
+
+// ******************************************************************************* //
